5157ee7879e02601282127902548cb85d5b7d712
Al <R@example.com>
2024-12-08 22:31:46 +0700

    Update10

--- src/mortgage_calculator.py
+++ src/mortgage_calculator.py
@@ -27,5 +27,5 @@ payment = calculate_monthly_payment(principal, annual_rate, years)
 
 # Старый способ форматирования строк для Python 3.5
 print("Your monthly payment is: {:.2f}".format(payment))
-print("Updated version 6: Your monthly payment is: 1432.25")
+print("Updated version 7: Your monthly payment is: 1432.25")
 
